3a26a6d8e115ad8cf64c911455ccb0fc389e8ee1
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2013-03-28 22:43:09 -0400

    implement a simple dialogue event

--- main.lua
+++ main.lua
@@ -5,11 +5,34 @@ ATL.path = "maps/"
 local map
 local commander = {}
 local enemy = {}
+local dialogue
 
 function drawSprite(sprite)
     love.graphics.draw(sprite.image, sprite.pos.x, sprite.pos.y,
         sprite.r, sprite.sx, sprite.sy,
         sprite.ox, sprite.oy)
+end
+
+local leftPortrait = {x = 15, y = 340, w = 100, h = 125,
+                      sx = -1, sy = 1, ox = 180, oy = 70}
+leftPortrait.stencil = love.graphics.newStencil(function()
+   love.graphics.rectangle("fill", leftPortrait.x, leftPortrait.y,
+                           leftPortrait.w, leftPortrait.h)
+end)
+
+local rightPortrait = {x = 525, y = 340, w = 100, h = 125,
+                       sx = 1, sy = 1, ox = 80, oy = 70}
+rightPortrait.stencil = love.graphics.newStencil(function()
+    love.graphics.rectangle("fill", rightPortrait.x, rightPortrait.y,
+                            rightPortrait.w, rightPortrait.h)
+end)
+
+function drawPortrait(sprite, portrait)
+    love.graphics.setStencil(portrait.stencil)
+    love.graphics.rectangle("fill", 0, 0, 640, 480)
+    love.graphics.draw(sprite.image, portrait.x, portrait.y,
+        portrait.r, portrait.sx, portrait.sy, portrait.ox, portrait.oy)
+    love.graphics.setStencil()
 end
 
 function updatePlayer(dt)
@@ -33,6 +56,21 @@ end
 function updateEnemy(dt)
     -- Move towards the player
     enemy.pos = enemy.pos + ((commander.pos - enemy.pos):normalized() * dt * enemy.speed)
+    if not enemy.spoke and (enemy.pos - commander.pos):len() < 5 then
+        local script = [[Commander: Ahoy there!
+Enemy: Sup!
+Commander: Not much.
+Enemy: Aiight.
+]]
+        dialogue = {
+            right = commander,
+            left = enemy,
+            script = script:gmatch('(.-)\n()'),
+            done = false,
+        }
+        dialogue.currentLine = dialogue.script()
+        enemy.spoke = true
+    end
 end
 
 function love.load()
@@ -47,20 +85,42 @@ function love.load()
     enemy.pos = vector(162, 162)
     enemy.sx = -1/8
     enemy.sy = 1/8
-    enemy.ox = 32
+    enemy.ox = 256
     enemy.oy = 0
     enemy.speed = 20
+    enemy.spoke = false
 end
 
 function love.update(dt)
-    updatePlayer(dt)
-    updateEnemy(dt)
+    if not dialogue then
+        updatePlayer(dt)
+        updateEnemy(dt)
+    end
+end
+
+function love.keyreleased(key)
+    if dialogue and key == "return" then
+        dialogue.currentLine = dialogue.script()
+        if not dialogue.currentLine then
+            dialogue.done = true
+        end
+    end
 end
 
 function love.draw()
     map:draw()
     drawSprite(commander)
     drawSprite(enemy)
+    if dialogue then
+        drawPortrait(dialogue.right, rightPortrait)
+        drawPortrait(dialogue.left, leftPortrait)
+        if dialogue.done then
+            dialogue = nil
+        elseif dialogue.currentLine then
+            love.graphics.print(dialogue.currentLine, 150, 430)
+        end
+    end
+
     love.graphics.print(string.format(
 [[Memory: %dKB
 Pos: (%f, %f)
