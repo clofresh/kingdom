c1e3b523a45741a34c7590e53bca0bfc4a204e62
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2013-03-31 22:19:34 -0400

    add publish task to upload to rackspace

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+source 'http://gemcutter.org/'
+gem 'rake'
+gem 'cloudfiles'
+
--- Rakefile.rb
+++ Rakefile.rb
@@ -2,6 +2,10 @@ def version()
     %x[cat VERSION].strip
 end
 
+def lovefile()
+    %x[echo build/${PWD##*/}-$(cat VERSION).love].strip
+end
+
 directory "build"
 
 task :submodules do
@@ -10,10 +14,23 @@ end
 
 task :compile => [:submodules, "build"] do
     sh <<-EOS
-        OUTPUT=build/${PWD##*/}-#{version}.love
+        OUTPUT=#{lovefile}
         rm $OUTPUT
         zip -r $OUTPUT * --exclude \*.acorn build/\* \*/.\*
     EOS
+end
+
+task :publish => [:compile] do
+    require 'cloudfiles'
+    require 'creds'
+
+    cf = CloudFiles::Connection.new(:username => RACKSPACE_USER,
+                                    :api_key  => RACKSPACE_API_KEY)
+    container = cf.container('games')
+    filename = lovefile
+    object = container.create_object File.basename(filename), false
+    object.load_from_filename filename
+    puts object.public_url
 end
 
 task :clean do
