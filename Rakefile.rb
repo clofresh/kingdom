1a26d0a647924b0e0ca352ada9cfc6aaf642a844
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2013-04-08 22:21:22 -0400

    add rake task dist:osx to build an osx .app

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,24 @@
+def name()
+    "kingdom"
+end
+
 def version()
     %x[cat VERSION].strip
 end
 
-def lovefile()
-    %x[echo build/${PWD##*/}-$(cat VERSION).love].strip
+def builddir()
+    "build"
 end
 
-directory "build"
+def lovefile()
+    "#{name}-#{version}.love"
+end
+
+def loveapp()
+    "/Applications/love.app"
+end
+
+directory builddir
 
 desc 'Initialize and update the submodule dependencies'
 task :submodules do
@@ -14,12 +26,22 @@ task :submodules do
 end
 
 desc 'Compile a .love file'
-task :compile => [:submodules, "build"] do
+task :compile => [:submodules, builddir] do
     sh <<-EOS
-        OUTPUT=#{lovefile}
+        OUTPUT=#{builddir}/#{lovefile}
         rm $OUTPUT
-        zip -r $OUTPUT * --exclude \*.acorn build/\* \*/.\*
+        zip -r $OUTPUT * --exclude \*.acorn #{builddir}/\* \*/.\*
     EOS
+end
+
+namespace :dist do
+    desc 'Bundle the .love file for OS X'
+    task :osx => [:clean, :compile] do
+        sh "cp -r #{loveapp} #{builddir}/"
+        sh "cp #{builddir}/#{lovefile} #{builddir}/love.app/Contents/Resources/"
+        sh "cp etc/Info.plist #{builddir}/love.app/Contents/"
+        sh "mv #{builddir}/love.app #{builddir}/#{name}-#{version}.app"
+    end
 end
 
 desc 'Compile and publish a .love file to the CDN'
@@ -30,7 +52,7 @@ task :publish => [:compile] do
     cf = CloudFiles::Connection.new(:username => RACKSPACE_USER,
                                     :api_key  => RACKSPACE_API_KEY)
     container = cf.container('games')
-    filename = lovefile
+    filename = "#{builddir}/#{lovefile}"
     object = container.create_object File.basename(filename), false
     object.load_from_filename filename
     puts object.public_url
@@ -38,7 +60,7 @@ end
 
 desc 'Clean out the build directory'
 task :clean do
-    sh "rm -rf build/*"
+    sh "rm -rf #{builddir}/*"
 end
 
 task :default => [:compile]
