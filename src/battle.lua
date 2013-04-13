d92e223f11f0364dc623c507fbca83ab5d4c06dd
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2013-04-12 23:23:03 -0400

    add randomWalk when units are in hitting distance, reorganize how battle state references units

--- src/battle.lua
+++ src/battle.lua
@@ -4,41 +4,60 @@ local Battle = {}
 function Battle:enter(prevState, leftArmy, rightArmy, nextState)
     battleCount = battleCount + 1
     audio.play(audio.songs.theme2)
+    self.round = 1
     self.index = sprite.SpatialIndex(32, 32)
-    self.leftOnScreen = sprite.SpatialIndex(640, 480)
-    self.rightOnScreen = sprite.SpatialIndex(640, 480)
+    self.onScreen = {
+        left  = sprite.SpatialIndex(640, 480),
+        right = sprite.SpatialIndex(640, 480),
+    }
 
     -- Lay out the troops on the battlefield
-    local leftDeploment = {}
-    local rightDeploment = {}
+    local units = {}
+    local active = {}
+    local teams = {left={}, right={}}
+    local leftDeploment = 0
+    local rightDeploment = 0
     local x, xDelta, y, yDelta
     x = 200
     xDelta = 200
     y = 10
     yDelta = 100
+    local team = 'left'
     for i, troop in pairs(leftArmy.troops) do
         troop.sx = -1/4
         troop.sy = 1/4
         troop.pos = vector(x, y)
         troop.tactic = 'advance'
-        table.insert(leftDeploment, troop)
+        troop.team = team
+        table.insert(units, troop)
+        local index = #units
+        table.insert(active, index)
+        teams[team][index] = index
         y = y + yDelta
+        leftDeploment = leftDeploment + 1
     end
-    print(leftArmy.name .. " has " .. #leftDeploment .. " troops")
+    print(leftArmy.name .. " has " .. leftDeploment .. " troops")
 
     x = x + xDelta
     y = 10
+    team = 'right'
     for i, troop in pairs(rightArmy.troops) do
         troop.sx = 1/4
         troop.sy = 1/4
         troop.pos = vector(x, y)
-        table.insert(rightDeploment, troop)
+        troop.team = team
+        table.insert(units, troop)
+        local index = #units
+        table.insert(active, index)
+        teams[team][index] = index
         y = y + yDelta
+        rightDeploment = rightDeploment + 1
     end
-    print(rightArmy.name .. " has " .. #rightDeploment .. " troops")
+    print(rightArmy.name .. " has " .. rightDeploment .. " troops")
 
-    self.leftDeploment = leftDeploment
-    self.rightDeploment = rightDeploment
+    self.units = units
+    self.active = active
+    self.teams = teams
     self.leftArmy = leftArmy
     self.rightArmy = rightArmy
     self.nextState = nextState or prevState
@@ -47,70 +66,77 @@ function Battle:enter(prevState, leftArmy, rightArmy, nextState)
 end
 
 function Battle:update(dt)
-
     -- Clear out defeated units
-    for i, unit in pairs(self.leftDeploment) do
+    local stillActive = {}
+    local teamCounts = {left=0, right=0}
+    for i, index in pairs(self.active) do
+        local unit = self.units[index]
         if unit.health <= 0 then
-            table.remove(self.leftDeploment, i)
+            self.units[index] = nil
+            self.teams[unit.team][index] = nil
             print(unit.name .. " is dead")
-        end
-    end
-    for i, unit in pairs(self.rightDeploment) do
-        if unit.health <= 0 then
-            table.remove(self.rightDeploment, i)
-            print(unit.name .. " is dead")
+        else
+            table.insert(stillActive, index)
+            teamCounts[unit.team] = teamCounts[unit.team] + 1
         end
     end
 
     -- Set tactics
-    self:updateTactics(self.leftDeploment, self.rightDeploment)
+    for i, index in pairs(stillActive) do
+        local unit = self.units[index]
+        local target, targetDistanceVec = self:findClosestEnemy(unit)
+        if targetDistanceVec == nil then
+            unit.tactic = 'halt'
+        elseif unit.tactic == 'advance' and targetDistanceVec:len() < 10 then
+            unit.tactic = 'randomWalk'
+        elseif unit.tactic == 'randomWalk' and targetDistanceVec:len() >= 10 then
+            unit.tactic = 'advance'
+        end
+    end
+    if #self.teams.left == 1 and #self.teams.right > 1 then
+        for i, index in pairs(self.teams.left) do
+            self.units[index].tactic = 'retreat'
+        end
+    end
 
     -- Resolve unit movement
-    for i, unit in pairs(self.leftDeploment) do
-        self:unitTactic(unit, dt, self.rightDeploment, self.leftDeploment)
+    for i, index in pairs(stillActive) do
+        local unit = self.units[index]
+        self:unitTactic(unit, dt)
         self.index:registerPos(unit)
-        self.leftOnScreen:registerPos(unit)
-    end
-    for i, unit in pairs(self.rightDeploment) do
-        self:unitTactic(unit, dt, self.leftDeploment, self.rightDeploment)
-        self.index:registerPos(unit)
-        self.rightOnScreen:registerPos(unit)
+        self.onScreen[unit.team]:registerPos(unit)
     end
 
     -- Resolve unit attacks
-    self.index:checkCollisions(dt, function(dt, sprites)
-        for i, sprt0 in pairs(sprites) do
-            for i, sprt1 in pairs(sprites) do
-                if sprt0 ~= sprt1 and math.random() > 0.5 then
-                    print(sprt0.name .. " hits " .. sprt1.name)
-                    sprt1.health = sprt1.health - 1
-                end
-            end
-        end
-    end)
+    for i, index in pairs(stillActive) do
+        local unit = self.units[index]
+        self:unitAttack(unit, dt)
+    end
     self.index:clear()
 
     -- Check for winning conditions
-    if #self.rightDeploment == 0 then
+    if teamCounts.right == 0 then
         print(self.leftArmy.name .. " defeated " .. self.rightArmy.name
               .. " in battle " .. battleCount)
         Gamestate.switch(self.nextState, self.leftArmy, self.rightArmy)
-    elseif #self.leftDeploment == 0 then
+    elseif teamCounts.left == 0 then
         print(self.rightArmy.name .. " defeated " .. self.leftArmy.name
               .. " in battle " .. battleCount)
         Gamestate.switch(self.nextState, self.rightArmy, self.leftArmy)
-    elseif not self.leftOnScreen:inBounds() then
+    elseif not self.onScreen.left:inBounds() then
         print(self.leftArmy.name .. " retreated from " .. self.rightArmy.name
               .. " in battle " .. battleCount)
         Gamestate.switch(self.nextState, self.rightArmy, nil)
-    elseif not self.rightOnScreen:inBounds() then
+    elseif not self.onScreen.right:inBounds() then
         print(self.rightArmy.name .. " retreated from " .. self.leftArmy.name
               .. " in battle " .. battleCount)
         Gamestate.switch(self.nextState, self.leftArmy, nil)
     end
 
-    self.leftOnScreen:clear()
-    self.rightOnScreen:clear()
+    self.onScreen.left:clear()
+    self.onScreen.right:clear()
+    self.active = stillActive
+    self.round = self.round + 1
 end
 
 function Battle:leave()
@@ -121,42 +147,65 @@ end
 function Battle:draw()
     love.graphics.setColor(255, 255, 255)
     love.graphics.rectangle("fill", 0, 0, 640, 480)
-    for i, troop in pairs(self.leftDeploment) do
-        sprite.draw(troop)
-    end
-    for i, troop in pairs(self.rightDeploment) do
-        sprite.draw(troop)
+    for i, index in pairs(self.active) do
+        sprite.draw(self.units[index])
     end
 end
 
-function Battle:updateTactics(friends, enemies)
-    if #friends == 1 and #enemies > 1 then
-        for i, unit in pairs(friends) do
-            unit.tactic = 'retreat'
-        end
-    end
-end
-
-function Battle:unitTactic(unit, dt, enemies, friends)
+function Battle:unitTactic(unit, dt)
     local tactic = unit.tactic or 'halt'
-    return tactics[tactic](self, unit, dt, enemies, friends)
+    return tactics[tactic](self, unit, dt)
+end
+
+function Battle:unitAttack(unit, dt)
+    local target, targetDistanceVec = self:findClosestEnemy(unit)
+    if target and targetDistanceVec:len() < 5 and math.random() > 0.5 then
+        local damage = 1
+        target.health = target.health - damage
+        print(string.format("R%d - [%s] %s hits [%s] %s for %d, %d health left", self.round, unit.team, unit.name, target.team, target.name, damage, target.health))
+    end
+    return
 end
 
 function Battle:keyreleased(key)
     if key == 'a' then
-        for i, unit in pairs(self.rightDeploment) do
-            unit.tactic = 'advance'
+        for i, index in pairs(self.teams.right) do
+            self.units[index].tactic = 'advance'
         end
     elseif key == 'h' then
-        for i, unit in pairs(self.rightDeploment) do
-            unit.tactic = 'halt'
+        for i, index in pairs(self.teams.right) do
+            self.units[index].tactic = 'halt'
         end
     elseif key == 'r' then
-        for i, unit in pairs(self.rightDeploment) do
-            unit.tactic = 'retreat'
+        for i, index in pairs(self.teams.right) do
+            self.units[index].tactic = 'retreat'
         end
     end
 end
+
+function Battle:findClosestEnemy(unit)
+    local target, targetDistance, targetDistanceVec
+    local newTargetDistance, newTargetDistanceVec
+    local enemies, enemy
+    if unit.team == 'left' then
+        enemies = self.teams.right
+    else
+        enemies = self.teams.left
+    end
+    -- Find the closest enemy
+    for i, index in pairs(enemies) do
+        enemy = self.units[index]
+        newTargetDistanceVec = enemy.pos - unit.pos
+        newTargetDistance = newTargetDistanceVec:len()
+        if target == nil or newTargetDistance < targetDistance then
+            target = enemy
+            targetDistance = newTargetDistance
+            targetDistanceVec = newTargetDistanceVec
+        end
+    end
+    return target, targetDistanceVec
+end
+
 
 return {
     state = Battle,
--- src/tactics.lua
+++ src/tactics.lua
@@ -1,42 +1,35 @@
-function findClosest(unit, neighbors)
-    local target, targetDistance, targetDistanceVec
-    local newTargetDistance, newTargetDistanceVec
-    -- Find the closest enemy
-    for i, neighbor in pairs(neighbors) do
-        newTargetDistanceVec = neighbor.pos - unit.pos
-        newTargetDistance = newTargetDistanceVec:len()
-        if target == nil or newTargetDistance < targetDistance then
-            target = neighbor
-            targetDistance = newTargetDistance
-            targetDistanceVec = newTargetDistanceVec
-        end
-    end
-    return target, targetDistanceVec
-end
-
 function moveTowards(unit, direction, dt)
     unit.pos = unit.pos + direction:normalize_inplace() * (unit.speed * dt)
 end
 
-function advance(battle, unit, dt, enemies, friends)
-    local target, targetDistanceVec = findClosest(unit, enemies)
+function randomVector()
+    return vector(math.random() - 0.5, math.random() - 0.5)
+end
+
+function advance(battle, unit, dt)
+    local target, targetDistanceVec = battle:findClosestEnemy(unit)
     if target then
         moveTowards(unit, targetDistanceVec, dt)
     end
 end
 
-function halt(battle, unit, dt, enemies, friends)
+function halt(battle, unit, dt)
 end
 
-function retreat(battle, unit, dt, enemies, friends)
-    local target, targetDistanceVec = findClosest(unit, enemies)
+function retreat(battle, unit, dt)
+    local target, targetDistanceVec = battle:findClosestEnemy(unit)
     if target then
         moveTowards(unit, -targetDistanceVec, dt)
     end
+end
+
+function randomWalk(battle, unit, dt)
+    moveTowards(unit, randomVector(), dt)
 end
 
 return {
     advance = advance,
     halt = halt,
     retreat = retreat,
+    randomWalk = randomWalk,
 }
